3c266dc96686f5685a713398698d6578f2d67221
Brandon Coats <34488021+b0coat01@users.noreply.github.com>
2019-02-01 16:54:55 -0500

    Add files via upload
    MCP3426 4 Channel with Explanation in Comments

---
+++ Python/SMBus_MCP3426.py
@@ -0,0 +1,57 @@
+# Distributed with a free-will license.
+# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+# MCP3426
+# This code is designed to work with the MCP3426_I2CADC I2C Mini Module available from ControlEverything.com.
+# https://www.controleverything.com/content/Analog-Digital-Converters?sku=MCP3426_I2CADC#tabs-0-product_tabset-2
+
+import smbus
+import time
+from time import sleep
+
+# Get I2C bus
+try:
+  bus = smbus.SMBus(1)
+except:
+  print("Failed to connect to MCP3426 serial manager bus")
+
+# Setup device and sensor settings for i2c communications
+sensor_read_intercept = 3.95
+sensor_read_slope = (39.5-sensor_read_intercept)/10
+
+address = 0x6e				#0x6e is the MCP3426 address Retrieved via cmd(sudo i2cdetect -y 1)
+ch1 = [0x10,(10/8.925),"ch1"]		#[0x10(00010000) is Continuous conversion mode on Channel-1 w 12-bit Resolution, Ch1 sensor calibration value]
+ch2 = [0x30,(10/8.925),"ch2"]		#[0x30(00110000) is Continuous conversion mode on Channel-2 w 12-bit Resolution, Ch2 sensor calibration value]
+ch3 = [0x50,(10/8.925),"ch3"]		#[0x50(01010000) is Continuous conversion mode on Channel-3 w 12-bit Resolution, Ch3 sensor calibration value]
+ch4 = [0x70,(10/8.925),"ch4"]		#[0x70(01110000) is Continuous conversion mode on Channel-4 w 12-bit Resolution, Ch4 sensor calibration value]
+
+def i2c_read_voltage(addr,ch):
+  # Send configuration command
+  try:
+    bus.write_byte(address, ch[0])
+  except:
+    print("Failed to connect to MCP3426")
+  # Sleep momentarily to allow settle
+  sleep(0.25)
+  # Read data back from 0x00(0), 2 bytes
+  data = bus.read_i2c_block_data(addr, 0x00, 2)
+  # Convert the data to 12-bits
+  bit_adc = (data[0] & 0x0F) * 256 + data[1]
+  if bit_adc > 2047 :
+    bit_adc -= 4095
+  # Convert and calibrate bit data to voltage
+  volt_adc = bit_adc/2047*10*(ch[1]) #2047 represents 10v signal with correction factor
+  # Output data to screen
+  print("ADC for Voltage ("+ch[2]+") : "+str(volt_adc))
+  return (volt_adc)
+
+
+while(1):
+        # Read voltage from ch1 sensor
+        sens_volt_adc = i2c_read_voltage(address, ch1)
+        # Read voltage from ch2 sensor
+        sens_volt_adc = i2c_read_voltage(address, ch2)
+        # Read voltage from ch3 sensor
+        sens_volt_adc = i2c_read_voltage(address, ch3)
+        # Read voltage from ch4 sensor
+        sens_volt_adc = i2c_read_voltage(address, ch4)
+                
